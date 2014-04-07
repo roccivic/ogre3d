0bf404e75a6f10317c0e027b5078aa18e5519c71
Rouslan Placella <rouslan@placella.com>
2014-04-07 21:47:05 +0100

    Tutorial 2

---
+++ BasicTutorial2/OgreApp1/src/OgreApp1.cpp
@@ -0,0 +1,110 @@
+#include "OgreApp1.h"
+
+//-------------------------------------------------------------------------------------
+OgreApp1::OgreApp1(void)
+{
+}
+ 
+//-------------------------------------------------------------------------------------
+OgreApp1::~OgreApp1(void)
+{
+}
+ 
+//-------------------------------------------------------------------------------------
+void OgreApp1::createCamera(void)
+{
+	// create the camera
+    mCamera = mSceneMgr->createCamera("PlayerCam");
+    // set its position, direction  
+    mCamera->setPosition(Ogre::Vector3(0,10,500));
+    mCamera->lookAt(Ogre::Vector3(0,0,0));
+    // set the near clip distance
+    mCamera->setNearClipDistance(5);
+ 
+    mCameraMan = new OgreBites::SdkCameraMan(mCamera);   // create a default camera controller
+}
+ 
+//-------------------------------------------------------------------------------------
+void OgreApp1::createViewports(void)
+{
+	// Create one viewport, entire window
+    Ogre::Viewport* vp = mWindow->addViewport(mCamera);
+    vp->setBackgroundColour(Ogre::ColourValue(0,0,0));
+    // Alter the camera aspect ratio to match the viewport
+    mCamera->setAspectRatio(Ogre::Real(vp->getActualWidth()) / Ogre::Real(vp->getActualHeight())); 
+}
+ 
+//-------------------------------------------------------------------------------------
+void OgreApp1::createScene(void)
+{
+    mSceneMgr->setAmbientLight(Ogre::ColourValue(0.3, 0.3, 0.3));
+    mSceneMgr->setShadowTechnique(Ogre::SHADOWTYPE_STENCIL_MODULATIVE);
+ 
+    Ogre::Entity* entNinja = mSceneMgr->createEntity("Ninja", "ninja.mesh");
+    entNinja->setCastShadows(true);
+    mSceneMgr->getRootSceneNode()->createChildSceneNode()->attachObject(entNinja);
+ 
+    Ogre::Plane plane(Ogre::Vector3::UNIT_Y, 0);
+ 
+    Ogre::MeshManager::getSingleton().createPlane("ground", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
+        plane, 1500, 1500, 20, 20, true, 1, 5, 5, Ogre::Vector3::UNIT_Z);
+ 
+    Ogre::Entity* entGround = mSceneMgr->createEntity("GroundEntity", "ground");
+    mSceneMgr->getRootSceneNode()->createChildSceneNode()->attachObject(entGround);
+ 
+    entGround->setMaterialName("Examples/Rockwall");
+    entGround->setCastShadows(false);
+ 
+    Ogre::Light* pointLight = mSceneMgr->createLight("pointLight");
+    pointLight->setType(Ogre::Light::LT_POINT);
+    pointLight->setPosition(Ogre::Vector3(0, 150, 250));
+ 
+    pointLight->setDiffuseColour(1.0, 0.5, 0.5);
+    pointLight->setSpecularColour(1.0, 0.5, 0.5);
+ 
+    Ogre::Light* spotLight = mSceneMgr->createLight("spotLight");
+    spotLight->setType(Ogre::Light::LT_SPOTLIGHT);
+    spotLight->setDiffuseColour(0, 1.0, 1.0);
+    spotLight->setSpecularColour(0, 1.0, 1.0);
+ 
+    spotLight->setDirection(-1, -1, 0);
+    spotLight->setPosition(Ogre::Vector3(300, 300, 0));
+ 
+    spotLight->setSpotlightRange(Ogre::Degree(50), Ogre::Degree(80));
+}
+
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+#define WIN32_LEAN_AND_MEAN
+#include "windows.h"
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+    INT WINAPI WinMain( HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT )
+#else
+    int main(int argc, char *argv[])
+#endif
+    {
+        // Create application object
+        OgreApp1 app;
+
+        try {
+            app.go();
+        } catch( Ogre::Exception& e ) {
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+            MessageBox( NULL, e.getFullDescription().c_str(), "An exception has occured!", MB_OK | MB_ICONERROR | MB_TASKMODAL);
+#else
+            std::cerr << "An exception has occured: " <<
+                e.getFullDescription().c_str() << std::endl;
+#endif
+        }
+
+        return 0;
+    }
+
+#ifdef __cplusplus
+}
+#endif
